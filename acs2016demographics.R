411ec5870f8f718c6df5e8af685ecd1081e5559e
Michael Fuller <fullerm@corp.tmacog.org>
2018-03-09 17:32:28 -0500

    update census variables; shiny app navbar;

--- acs2016demographics.R
+++ acs2016demographics.R
@@ -6,21 +6,45 @@ api_key = "b7da053b9e664586b9e559dba9e73780602f0aab"
 
 blk_grps_2010 <- c(2,3)
 
+listCensusMetadata(name = "acs/acs5",vintage = 2016, type = "variables")
 #What variables do I want?
 #race
+vars <- c("B03002_003E","B03002_003M", #white alone, non-hispanic
+          "B06011_001E","B06011_001M", #median income
+          "B15003_017E","B15003_017M", #high school diploma
+          "B15003_018E","B15003_018M", #GED
+          "B15003_022E","B15003_022M", #bachelor's degree
+          "B17017_001E","B17017_001M", #households
+          "B17017_002E","B17017_002M", #households below poverty level
+          "B25001_001E","B25001_001M", #housing units
+          "B25002_003E","B25002_003M", #vacant housing units
+          "B25003_003E","B25003_003M", #renter occupied housing units
+          "B25031_001E","B25031_001M", #median gross rent
+          "B25010_002E","B25010_001M", #average household size 
+          "B25010_002E","B25010_002M", #average household size for owner occupied
+          "B25010_003E","B25010_003M", #average household size for renter occupied
+          "B23025_001E","B23025_001M", #16 and older population 
+          "B23025_005E","B23025_005M", #unemployed
+          "B23025_007E","B23025_007M", #not in labor force
+          "B23025_005E","B23025_005M", #unemployed
+          
+)
 #income
 #pop pyramid of  2016 data?
 #  B01001_003E:B01001_049E
 #poverty
 #home ownership
 #vehicle ownership
+#median gross rent
+#education
+#household size
 
 ## Acquire and tidy 2000 block data
 blkgrp2016data <- tibble()
 #for (i in unique(substr(blks_2010$fips,6,11))){
 temp <-as_tibble(getCensus(name="acs/acs5", 
                              vintage = 2016, 
-                             vars =  "B01001_003E", 
+                             vars =  c("B01001_003E", "B01001_003M"),
                              region = "block group:*",##,paste(substr(blks_2000$fips,12,15),collapse = ','),sep = ""), 
                              regionin = "state:39+county:095+tract:010300",                        
                              key = api_key ))
--- middle-grounds-demographics/app.R
+++ middle-grounds-demographics/app.R
@@ -11,70 +11,121 @@ library(shiny)
 library(leaflet)
 library(shinydashboard)
 library(rgdal)
+library(png)
+#pyramid2000 <- readPNG("data/pyramid2000.png")
 blk2000 <- readOGR("data/middle_grounds_blk_2000.gpkg")
 blk2000 <- spTransform(blk2000, CRS("+init=epsg:4326"))
 
 # Define UI for application that draws a histogram
 ui <- #dashboardPage(
-  bootstrapPage(
-  tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
+  navbarPage("Middle Grounds District",
+             tabPanel("Home",
+                      #sidebarLayout(
+                        #sidebarPanel(div(img(src = "middle-grounds-aerial.jpg"),
+                                     #    "Middle Grounds District Demographic Data and Trends") ,
+                                     #img(src = "TMACOGlogo.jpg") )
+                      
+                      mainPanel(p("This app supplies demographic data to support the Middle Grounds District Plan"),
+                                p("The plan is available ", a(href="http://toledo.oh.gov/media/4015/middle-grounds-district-plan-2015-reduced.pdf", "here")),
+                                h2("Middle Grounds District Legal Description"),
+                                p("The Middle Grounds District boundary is as follows: Commencing at the intersection of 
+                                  the centerline of Clayton Street and the centerline of South St. Clair Street,
+                                  thence southwest along the centerline of South St. Clair Street to its intersection with 
+                                  the centerline of Newton Street,
+                                  thence west along the centerline of Newton Street to its intersection with the centerline 
+                                  of Collingwood Boulevard,
+                                  thence west along the centerline of Collingwood Boulevard to its intersection with the 
+                                  east right-of-way line of I-75,
+                                  thence southeast along the east right
+                                  -of-way of I-75 to a point on the centerline of the 
+                                  Norfolk Southern Railroad right-of-way, 
+                                  thence southeast along the centerline of the Norfolk Southern Railroad right-
+                                  of-way to a point being the centerline of the Maumee River,
+                                  thence downstream, north, along the centerline of the Maumee River to a point on the 
+                                  centerline of the Anthony Wayne Bridge (State Routes 2, 51,and 65),
+                                  thence northwest along the centerline of the Anthony Wayne Bridge westward to its 
+                                  intersection with the centerline of  Clayton Street,
+                                  thence west along the centerline of Clayton Street to its intersection the centerline of 
+                                  South St. Clair Street, the point of beginning."),
+                                leafletOutput("map"))
+                      ),
+             tabPanel("Population",
+                      mainPanel(imageOutput("pyramid"))),
+             tabPanel("Race",
+                      # Sidebar with a slider input for number of bins 
+                      sidebarLayout(   
+                        sidebarPanel(
+                          
+                          
+                          sliderInput("bins",
+                                      "Number of bins:",
+                                      min = 5,
+                                      max = 50,
+                                      value = 30),
+                          selectInput("select",h3("Select Data to view on the map"), 
+                                      choices = list("1990 Census" ,
+                                                     "2000 Census" ,
+                                                     "2010 Census" ,
+                                                     "2012-2016 American Community Survey",
+                                                     "2007-2011 American Community Survey" ), selected = NULL
+                          ),
+                          img(src = "TMACOGlogo.jpg"),
+                          p("Data complied by ",a(href = "http://www.tmacog.org","TMACOG")),
+                          
+                          
+                          p("Data from US Census Bureau")),
+                      mainPanel(tableOutput("table1")))),
+             tabPanel("Unemployment",
+                      mainPanel(tableOutput("unemployment"))),
+             tabPanel("Poverty",
+                      mainPanel(p("It is important to note that all block groups in the Middle Grounds District have a low-moderate income percentage of over 51%, so this area would be eligible to receive some of the City of Toledo's CDBG entitlement award from HUD."),
+                        tableOutput("poverty"))),
+             tabPanel("Summary", h1("First level title"),
+                      h2("Second level title"), h3("Third level title"), h4("Fourth level title"),  
+                      h5("Fifth level title"),h6("Sixth level title")),
+             tabPanel( "Trends",plotOutput("distPlot")),
+             tabPanel("Interactive Map", leafletOutput("map",height = 800)),
+             tabPanel("Education",
+                      mainPanel(tableOutput("education")))
+  )
+  #tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
   
-  fluidPage(
+  #fluidPage(
    
    # Application title
-   titlePanel(title = div(img(src = "middle-grounds-aerial.jpg"),
-                               "Middle Grounds District Demographic Data and Trends") ,
-                               img(src = "TMACOGlogo.jpg") ),
+   #titlePanel(title = div(img(src = "middle-grounds-aerial.jpg"),
+    #                           "Middle Grounds District Demographic Data and Trends") ,
+     #                          img(src = "TMACOGlogo.jpg") ),
    
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(   
-     sidebarPanel(
-     
-      
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 5,
-                     max = 50,
-                     value = 30),
-         selectInput("select",h3("Select Data to view on the map"), 
-                     choices = list("1990 Census" ,
-                                    "2000 Census" ,
-                                    "2010 Census" ,
-                                    "2012-2016 American Community Survey",
-                                    "2007-2011 American Community Survey" ), selected = NULL
-                     ),
-         img(src = "TMACOGlogo.jpg"),
-         p("Data complied by ",a(href = "http://www.tmacog.org","TMACOG")),
-         
-         
-         p("Data from US Census Bureau")),
+   
       
       # Show a plot of the generated distribution
-      mainPanel(
+      #mainPanel(
         #tags$style(type = "text/css", "#map {height: calc(100vh - 80 px) !important;}"),
         #mainPanel(
-        tabsetPanel(
+       # tabsetPanel(
           #tabItem(div(class = "outer",
             #tags$style(type = "text/css", "#map, .leaflet-zoom-animated, html, body {width: 100%;height:800}"),
                   #tabName = "Map",
-        tabPanel("Interactive Map", leafletOutput("map",height = 800)),#width = "100%", height = "100%")),
+        #width = "100%", height = "100%")),
         #tabPanel("Summary",tabName = "Summary"),
         #tabPanel("Trends", tabName = "Trends"),
             #div(class = "map",)
-          tabPanel("Summary", h1("First level title"),
-            h2("Second level title"), h3("Third level title"), h4("Fourth level title"),  
-            h5("Fifth level title"),h6("Sixth level title")),
-         tabPanel( "Trends",plotOutput("distPlot"))
+          
+         
         
-      )
+      #)
    #)
-   )
+   #)
   #)
-)))
+#)))
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-   
+   output$pyramid <- renderImage({
+     
+     list(src = "images/pyramid2000.png")
+   },deleteFile = FALSE)
    output$distPlot <- renderPlot({
       # generate bins based on input$bins from ui.R
       x    <- faithful[, 2] 
